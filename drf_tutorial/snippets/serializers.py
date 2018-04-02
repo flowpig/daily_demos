1227eb7f012d0f1460308c474f87cd0b0398a2ea
flowpig <2802703843@qq.com>
2018-04-02 15:16:13 +0800

    use simple serializers

---
+++ drf_tutorial/snippets/serializers.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+'''
+python manage.py shell
+---------------------------------------------------------------
+# 写一些snippets代码来使序列化工作。
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+snippet = Snippet(code='foo = "bar"\n')
+snippet.save()
+
+snippet = Snippet(code='print "hello, world"\n')
+snippet.save()
+--------------------------------------------------------------
+# 序列化实例：
+
+serializer = SnippetSerializer(snippet)
+serializer.data
+# {'pk': 2, 'title': u'', 'code': u'print "hello, world"\n', 'linenos': False, 'language': u'python', 'style': u'friendly'}
+
+# 将数据渲染成json
+content = JSONRenderer().render(serializer.data)
+content
+# '{"pk": 2, "title": "", "code": "print \\"hello, world\\"\\n", "linenos": false, "language": "python", "style": "friendly"}'
+
+# 序列化querysets， 需要加参数many = True
+serializer = SnippetSerializer(Snippet.objects.all(), many=True)
+serializer.data
+# [OrderedDict([('pk', 1), ('title', u''), ('code', u'foo = "bar"\n'), ('linenos', False), ('language', 'python'), ('style', 'friendly')]), OrderedDict([('pk', 2), ('title', u''), ('code', u'print "hello, world"\n'), ('linenos', False), ('language', 'python'), ('style', 'friendly')]), OrderedDict([('pk', 3), ('title', u''), ('code', u'print "hello, world"'), ('linenos', False), ('language', 'python'), ('style', 'friendly')])]
+
+--------------------------------------------------------------
+# 反序列化也一样。首先，我们需要将流(stream)解析成Python原生数据类型
+
+serializer = SnippetSerializer(data=data)
+serializer.is_valid()
+# True
+serializer.validated_data
+# OrderedDict([('title', ''), ('code', 'print "hello, world"\n'), ('linenos', False), ('language', 'python'), ('style', 'friendly')])
+serializer.save()
+
+-------------------------------------------------------------
+
+
+
+'''
+
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    def create(self, validated_data):
+        '''
+        Create and return a new `Snippet` instance, given the validated data.
+        '''
+
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        '''
+        Update and return an existing `Snippet` instance, given the validated data.
+        '''
+
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance

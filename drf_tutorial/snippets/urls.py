6fc8a42ec529931fc22c66f1824b1f3671b02ae3
flowpig <2802703843@qq.com>
2018-04-08 18:33:30 +0800

    增加apis详见testview6.py

--- drf_tutorial/snippets/urls.py
+++ drf_tutorial/snippets/urls.py
@@ -2,7 +2,7 @@
 #-*- coding:utf-8 -*-
 
 from django.conf.urls import url, include
-from snippets import views, testview1, testview2, testview3, testview4, testview5
+from snippets import views, testview1, testview2, testview3, testview4, testview5, testview6
 
 
 urlpatterns = [
@@ -10,10 +10,11 @@ urlpatterns = [
     # url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
     # url(r'^snippets/$', testview1.snippet_list),
     # url(r'^snippets/(?P<pk>[0-9]+)/$', testview1.snippet_detail),
-    url(r'^snippets/$', testview5.SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', testview5.SnippetDetail.as_view()),
-    url(r'^users/$', testview5.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', testview5.UserDetail.as_view()),
+    url(r'^snippets/$', testview6.SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', testview6.SnippetDetail.as_view()),
+    url(r'^users/$', testview6.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', testview6.UserDetail.as_view()),
+    url(r'^$', testview6.api_root),
 ]
 
 urlpatterns += [

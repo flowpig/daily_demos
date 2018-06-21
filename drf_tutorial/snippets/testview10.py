bc813562aa8a44bfe2cdb31bae11729bd1b37513
flowpig <2802703843@qq.com>
2018-06-21 17:19:35 +0800

    在testview10.py增加排序demo

--- drf_tutorial/snippets/testview10.py
+++ drf_tutorial/snippets/testview10.py
@@ -6,6 +6,10 @@ from rest_framework import permissions
 from snippets.permission import IsOwnerOrReadOnly
 from rest_framework.decorators import list_route, detail_route
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from django.db.models.query import QuerySet
+import operator
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -28,6 +32,25 @@ class SnippetViewSet(viewsets.ModelViewSet):
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    ordering_fields = ('id',)       # ?ordering=id   ?ordering=-id
+    # 如果serializer类的SerializerMethod字段可以在返回的response['result']里进行sort排序
+
+    # def filter_queryset(self, queryset):
+    #     queryset = super(SnippetViewSet, self).filter_queryset(queryset)
+    #     return queryset.order_by('id')
+
+    def list(self, request, *args, **kwargs):
+        response = super(SnippetViewSet, self).list(request, *args, **kwargs)
+        ordering = request.query_params.get('ordering')    # url
+        print(response.data)
+        # response.data['results'] = sorted(response.data['results'], key=operator.itemgetter(ordering.replace('-', ''), ))
+        x = 1
+        if "-" in ordering:
+            response.data = sorted(response.data, key=lambda k: (k[ordering.replace('-', '')],), reverse=True)
+        else:
+            response.data = sorted(response.data, key=lambda k: (k[ordering],))
+        return response
 
     @list_route(methods=['GET'])
     def get_tt(self, request):
